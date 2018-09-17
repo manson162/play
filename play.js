32e1b94aa3dea953bbfd4d0c3ff47bc357264555
manson162 <kevin.torrington@gmail.com>
2018-09-17 23:04:40 +0100

    API not working

--- play.js
+++ play.js
@@ -1,3 +1,10 @@
-function buttonClick(){
-    document.body.style.backgroundColor = "red";
-}
+const url='http://api.electrika.com/api/DatasheetsPdf?brandNodeId=119288&partNumber=CFP702-4';
+
+fetch(url)
+.then(data=>{return data.json()})
+.then(res=>{console.log(res)})
+
+
+document.getElementById("addUrl").href = url.data;
+
+document.getElementById("addUrl").innerHTML = "Datasheet";
