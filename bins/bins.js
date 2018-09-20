7819b20d76955aa8dc17e325adffea4992c9c407
manson162 <kevin.torrington@gmail.com>
2018-09-20 23:00:50 +0100

    Tidy code

--- bins/bins.js
+++ bins/bins.js
@@ -1,5 +1,5 @@
 //Get ISO week number
-Date.prototype.getWeek = function() {
+Date.prototype.getWeek = function () {
     var onejan = new Date(this.getFullYear(), 0, 1);
     return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
 }
@@ -9,84 +9,73 @@ var weekNumber = (new Date()).getWeek();
 var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 var now = new Date();
 document.write(dayNames[now.getDay()] + " (" + weekNumber + ").");
-//End ISO week
 
-switch(weekNumber){
+
+//Display bins due to go out
+
+switch (weekNumber) {
     case 38:
-    document.getElementById("blueBin").style.display = "flex";
-    document.getElementById("greenBin").style.display = "flex";
-    break;
+        blueGreen()
+        break;
     case 39:
-    document.getElementById("blackBin").style.display = "flex";
-    document.getElementById("brownBin").style.display = "flex";
-    document.getElementById("greenBin").style.display = "flex";
-    break;
+        allBins()
+        break;
     case 40:
-    document.getElementById("blueBin").style.display = "flex";
-    document.getElementById("greenBin").style.display = "flex";
-    break;
+        blueGreen()
+        break;
     case 41:
-    document.getElementById("blackBin").style.display = "flex";
-    document.getElementById("brownBin").style.display = "flex";
-    document.getElementById("greenBin").style.display = "flex";
-    break;
+        allBins()
+        break;
     case 42:
-    document.getElementById("blueBin").style.display = "flex";
-    document.getElementById("greenBin").style.display = "flex";
-    break;
+        blueGreen();
+        break;
     case 43:
-    document.getElementById("blackBin").style.display = "flex";
-    document.getElementById("brownBin").style.display = "flex";
-    document.getElementById("greenBin").style.display = "flex";
-    break;
+        allBins()
+        break;
     case 44:
-    document.getElementById("blueBin").style.display = "flex";
-    break;
+        onlyBlue()
+        break;
     case 45:
-    document.getElementById("blackBin").style.display = "flex";
-    document.getElementById("brownBin").style.display = "flex";
-    document.getElementById("greenBin").style.display = "flex";
-    break;
+        allBins()
+        break;
     case 46:
-    document.getElementById("blueBin").style.display = "flex";
-    break;
+        onlyBlue()
+        break;
     case 47:
-    document.getElementById("blackBin").style.display = "flex";
-    document.getElementById("brownBin").style.display = "flex";
-    document.getElementById("greenBin").style.display = "flex";
-    break;
+        allBins()
+        break;
     case 48:
-    document.getElementById("blueBin").style.display = "flex";
-    break;
+        onlyBlue()
+        break;
     case 49:
-    document.getElementById("blackBin").style.display = "flex";
-    document.getElementById("brownBin").style.display = "flex";
-    document.getElementById("greenBin").style.display = "flex";
-    break;
+        allBins()
+        break;
     case 50:
-    document.getElementById("blueBin").style.display = "flex";
-    break;
+        onlyBlue()
+        break;
     case 51:
+        allBins()
+        break;
+    case 52:
+        onlyBlue()
+        break;
+
+}
+
+// Bin combos
+
+function blueGreen() {
+    document.getElementById("blueBin").style.display = "flex";
+    document.getElementById("greenBin").style.display = "flex";
+}
+
+function allBins() {
     document.getElementById("blackBin").style.display = "flex";
     document.getElementById("brownBin").style.display = "flex";
     document.getElementById("greenBin").style.display = "flex";
-    break;
-    case 52:
+}
+
+function onlyBlue() {
     document.getElementById("blueBin").style.display = "flex";
-    break;
-
 }
 
-
-
-
-/*
-if (weekNumber == 38) {
-
-    document.getElementById("blueBin").style.display = "flex";
-    document.getElementById("greenBin").style.display = "flex";
-}
-else  if (weekNumber == 39) {
-    document.getElementById("blackBin").style.display = "flex";
-}
-*/
